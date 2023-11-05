4f084e3ec7d75a150ee4a477682a85c89dba9c9f
ilya <34188710+illiaSoroka@users.noreply.github.com>
2023-11-05 09:50:12 -0600

    add

--- installation.sh
+++ installation.sh
@@ -2,3 +2,4 @@
 git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
 echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile
 echo 'export PATH=$PATH:$HOME/.tfenv/bin' >> ~/.bashrc
+ln -s ~/.tfenv/bin/* /usr/local/bin
