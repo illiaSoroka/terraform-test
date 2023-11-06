96b6674e119097ece878c92f7a73fd965656c307
ilya <34188710+illiaSoroka@users.noreply.github.com>
2023-11-05 19:43:12 -0600

    added membersip

--- Class1/main.tf
+++ Class1/main.tf
@@ -8,3 +8,14 @@ resource "aws_iam_user" "august23" {
 resource "aws_iam_group" "devops" {
   name = "devops"
 }
+
+
+resource "aws_iam_group_membership" "devops-team" {
+  name = "devops-team"
+
+  users = [
+    aws_iam_user.august23.name,
+  ]
+
+  group = aws_iam_group.devops.name
+}
