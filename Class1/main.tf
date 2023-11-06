57f25bfe4d0a8ed31ceb50ad605dd875cd305207
ilya <34188710+illiaSoroka@users.noreply.github.com>
2023-11-05 19:51:01 -0600

    Creates multiple users

--- Class1/main.tf
+++ Class1/main.tf
@@ -1,3 +1,5 @@
+
+# Create a user
 resource "aws_iam_user" "august23" {
   name = "august23"
   tags = {
@@ -5,11 +7,12 @@ resource "aws_iam_user" "august23" {
   }
 }
 
+# Create a group
 resource "aws_iam_group" "devops" {
   name = "devops"
 }
 
-
+# Creates group membership
 resource "aws_iam_group_membership" "devops-team" {
   name = "devops-team"
 
@@ -18,4 +21,15 @@ resource "aws_iam_group_membership" "devops-team" {
   ]
 
   group = aws_iam_group.devops.name
+}
+
+# Creates multiple users
+resource "aws_iam_user" "multiuser" {
+    name = each.key 
+    for_each = toset([
+        "bob",
+        "sam",
+        "lisa",
+    ])
+
 }
