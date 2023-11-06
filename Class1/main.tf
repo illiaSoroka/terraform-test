4fe47ea08545beedf9328db0db980d1808a862eb
ilya <34188710+illiaSoroka@users.noreply.github.com>
2023-11-05 20:05:52 -0600

    add users list

--- Class1/main.tf
+++ Class1/main.tf
@@ -3,7 +3,7 @@
 resource "aws_iam_user" "august23" {
   name = "august23"
   tags = {
-  tag-key = "Dev"
+    tag-key = "Dev"
   }
 }
 
@@ -25,11 +25,11 @@ resource "aws_iam_group_membership" "devops-team" {
 
 # Creates multiple users
 resource "aws_iam_user" "multiuser" {
-    name = each.key 
-    for_each = toset([
-        "bob",
-        "sam",
-        "lisa",
-    ])
+  name = each.key
+  for_each = toset([
+    "bob",
+    "sam",
+    "lisa",
+  ])
 
 }
