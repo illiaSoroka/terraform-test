e1588a163622605ce086be80217b7b4d166d33a7
ilya <34188710+illiaSoroka@users.noreply.github.com>
2023-11-08 11:16:08 -0600

    added sec group

--- Class2/main.tf
+++ Class2/main.tf
@@ -20,9 +20,47 @@ resource "aws_instance" "web" {
   ami             = data.aws_ami.ubuntu.id
   instance_type   = "t3.micro"
   key_name        = aws_key_pair.class2.key_name
+  vpc_security_group_ids = [
+    aws_security_group.class2-sec-group.id
+    ]
 }
 
+
+# Create ec2 key pair
 resource "aws_key_pair" "class2" {
   key_name        = "class2-key"
   public_key      = file("~/.ssh/id_rsa.pub")
+}
+
+# Create ec2 security group
+
+resource "aws_security_group" "class2-sec-group" {
+  name        = "class2-sec-group"
+  description = "Allow TLS inbound traffic"
+  
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 }
