aed45d2b3c5616a0820da071b3e5b1d2e608c65a
ilya <34188710+illiaSoroka@users.noreply.github.com>
2023-11-23 16:48:23 -0600

    key-pair

--- Class5/main.tf
+++ Class5/main.tf
@@ -2,3 +2,8 @@ resource "aws_key_pair" "class5" {
   key_name = "class5"
   public_key = file("~/.ssh/id_rsa.pub")
 }
+resource "aws_key_pair" "august23-test5" {
+  key_name = "august23-test5"
+  public_key = file("~/.ssh/id_rsa.pub")
+  
+}
