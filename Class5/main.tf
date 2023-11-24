6f80527d138750a8046648250a6d62b1f560de2d
ilya <34188710+illiaSoroka@users.noreply.github.com>
2023-11-23 18:35:38 -0600

    added az

--- Class5/main.tf
+++ Class5/main.tf
@@ -1,14 +1,21 @@
+
 resource "aws_key_pair" "class5" {
   key_name = "class5"
   public_key = file("~/.ssh/id_rsa.pub")
 }
+
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  azs             = [
+    data.aws_availability_zones.available.names[0],
+    data.aws_availability_zones.available.names[1],
+    data.aws_availability_zones.available.names[1],
+  ]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
@@ -20,3 +27,28 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+module "vpc-california" {
+  source = "terraform-aws-modules/vpc/aws"
+  count = var.region == "california" ? 1 : 0
+
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = [
+    data.aws_availability_zones.available.names[0],
+    data.aws_availability_zones.available.names[1],
+  ]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24",]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
