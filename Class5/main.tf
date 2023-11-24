dd78a4fb7a43e14513bffb2ab0edce9f5400895d
ilya <34188710+illiaSoroka@users.noreply.github.com>
2023-11-23 18:11:25 -0600

    add vpc

--- Class5/main.tf
+++ Class5/main.tf
@@ -2,3 +2,21 @@ resource "aws_key_pair" "class5" {
   key_name = "class5"
   public_key = file("~/.ssh/id_rsa.pub")
 }
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
